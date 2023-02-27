48addcc0558a71e745f32ab40afce9acd807cd75
aekasit-mo <aekasit.mo@chococrm.com>
2023-02-27 10:22:00 +0000

    todo openCV

---
+++ 1_👋_Homepage.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Welcome to My App.",
+    page_icon="👋",
+)
+
+st.title("Welcome to My App.")
+st.sidebar.success("Select menu above.")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,34 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 App Name')
 
-st.write('Hello world!')
+st.subheader('Input')
+url_input = st.text_input('GitHub URL', '')
+
+if url_input:
+    st.subheader('Output')
+    st.info(f'The GitHub URL of your data is: {url_input}')
+    df = pd.read_csv(url_input)
+    st.write(df)
+
+    column_name = df.columns
+    df2 = df.groupby(column_name[-1]).mean()
+    st.write(df2)
+    st.bar_chart(df2)
+
+else:
+    st.header('Enter your input')
+    st.error('Awaiting your input!')
+
+# species = df.columns[column_name[-1]]
+# st.write(species)
+
+genre = st.radio(
+    "What\'s your favorite movie genre",
+    ('Comedy', 'Drama', 'Documentary'))
+
+if genre == 'Comedy':
+    st.write('You selected comedy.')
+else:
+    st.write("You didn\'t select comedy.")
